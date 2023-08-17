bcb1ed257db08e40ea79eb99355c0dde52584938
devMorshed <ac3m0r@gmail.com>
2023-08-17 15:54:58 +0600

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <main className="my-10 px-10">
+      <section>
+        <h1 className="text-5xl text-center uppercase font-bold tracking-wide">
+          Welcome to XYZ Engine
+        </h1>
+      </section>
+    </main>
+  );
+};
+
+export default App;
